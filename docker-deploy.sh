498e3acb94d78bcca2a5e36210e883972b8d88f7
HamedKaramoko <mydalmajor18@gmail.com>
2019-01-08 19:51:42 +0100

    Docker-deploy.sh updated.

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,2 +1,2 @@
 docker login --username hamedkaramoko --password Mamanjetaime92@
-docker run --rm hamedkaramoko/continuous-integration:1.0
+docker run --rm -d hamedkaramoko/continuous-integration:1.0
